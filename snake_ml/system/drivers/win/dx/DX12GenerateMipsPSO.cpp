9bd60c8bd0c71f77c5147750e4aeba8d046f4c9a
alex.mrev1l1.kurinnoi <oleksandr.kurinnoi@gmail.com>
2021-03-08 22:06:13 +0000

    [Refactoring] : Fixed indenting.

--- snake_ml/system/drivers/win/dx/DX12GenerateMipsPSO.cpp
+++ snake_ml/system/drivers/win/dx/DX12GenerateMipsPSO.cpp
@@ -15,67 +15,67 @@ namespace win
 
 DX12GenerateMipsPSO::DX12GenerateMipsPSO()
 {
-    DX12Driver* dx12Driver = ((DX12Driver*)DX12Driver::GetInstance());
-    auto device = dx12Driver->GetD3D12Device();
+	DX12Driver* dx12Driver = ((DX12Driver*)DX12Driver::GetInstance());
+	auto device = dx12Driver->GetD3D12Device();
 
-    D3D12_FEATURE_DATA_ROOT_SIGNATURE featureData = {};
-    featureData.HighestVersion = D3D_ROOT_SIGNATURE_VERSION_1_1;
-    if ( FAILED( device->CheckFeatureSupport( D3D12_FEATURE_ROOT_SIGNATURE, &featureData, sizeof( featureData ) ) ) )
-    {
-        featureData.HighestVersion = D3D_ROOT_SIGNATURE_VERSION_1_0;
-    }
+	D3D12_FEATURE_DATA_ROOT_SIGNATURE featureData = {};
+	featureData.HighestVersion = D3D_ROOT_SIGNATURE_VERSION_1_1;
+	if ( FAILED( device->CheckFeatureSupport( D3D12_FEATURE_ROOT_SIGNATURE, &featureData, sizeof( featureData ) ) ) )
+	{
+		featureData.HighestVersion = D3D_ROOT_SIGNATURE_VERSION_1_0;
+	}
 
-    constexpr UINT mipsNum = 4;
-    CD3DX12_DESCRIPTOR_RANGE1 srcMip( D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 0, 0, D3D12_DESCRIPTOR_RANGE_FLAG_DESCRIPTORS_VOLATILE );
-    CD3DX12_DESCRIPTOR_RANGE1 outMip( D3D12_DESCRIPTOR_RANGE_TYPE_UAV, mipsNum, 0, 0, D3D12_DESCRIPTOR_RANGE_FLAG_DESCRIPTORS_VOLATILE );
+	constexpr UINT mipsNum = 4;
+	CD3DX12_DESCRIPTOR_RANGE1 srcMip( D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 0, 0, D3D12_DESCRIPTOR_RANGE_FLAG_DESCRIPTORS_VOLATILE );
+	CD3DX12_DESCRIPTOR_RANGE1 outMip( D3D12_DESCRIPTOR_RANGE_TYPE_UAV, mipsNum, 0, 0, D3D12_DESCRIPTOR_RANGE_FLAG_DESCRIPTORS_VOLATILE );
 
-    CD3DX12_ROOT_PARAMETER1 rootParameters[GenerateMips::NumRootParameters] = {};
-    rootParameters[GenerateMips::GenerateMipsCB].InitAsConstants(sizeof(DX12GenerateMipsCB) / sizeof(DX12GenerateMipsCB::Padding), 0 );
-    rootParameters[GenerateMips::SrcMip].InitAsDescriptorTable( 1, &srcMip );
-    rootParameters[GenerateMips::OutMip].InitAsDescriptorTable( 1, &outMip );
+	CD3DX12_ROOT_PARAMETER1 rootParameters[GenerateMips::NumRootParameters] = {};
+	rootParameters[GenerateMips::GenerateMipsCB].InitAsConstants(sizeof(DX12GenerateMipsCB) / sizeof(DX12GenerateMipsCB::Padding), 0 );
+	rootParameters[GenerateMips::SrcMip].InitAsDescriptorTable( 1, &srcMip );
+	rootParameters[GenerateMips::OutMip].InitAsDescriptorTable( 1, &outMip );
 
-    CD3DX12_STATIC_SAMPLER_DESC linearClampSampler( 0,
-                                                    D3D12_FILTER_MIN_MAG_MIP_LINEAR,
-                                                    D3D12_TEXTURE_ADDRESS_MODE_CLAMP,
-                                                    D3D12_TEXTURE_ADDRESS_MODE_CLAMP,
-                                                    D3D12_TEXTURE_ADDRESS_MODE_CLAMP
-    );
+	CD3DX12_STATIC_SAMPLER_DESC linearClampSampler( 0,
+													D3D12_FILTER_MIN_MAG_MIP_LINEAR,
+													D3D12_TEXTURE_ADDRESS_MODE_CLAMP,
+													D3D12_TEXTURE_ADDRESS_MODE_CLAMP,
+													D3D12_TEXTURE_ADDRESS_MODE_CLAMP
+	);
 
-    CD3DX12_VERSIONED_ROOT_SIGNATURE_DESC rootSignatureDesc( GenerateMips::NumRootParameters,
-                                                             rootParameters, 1, &linearClampSampler );
+	CD3DX12_VERSIONED_ROOT_SIGNATURE_DESC rootSignatureDesc( GenerateMips::NumRootParameters,
+															 rootParameters, 1, &linearClampSampler );
 
-    m_rootSignature.SetRootSignatureDesc( rootSignatureDesc.Desc_1_1, featureData.HighestVersion );
+	m_rootSignature.SetRootSignatureDesc( rootSignatureDesc.Desc_1_1, featureData.HighestVersion );
 
-    // Create the PSO for GenerateMips shader.
-    struct PipelineStateStream
-    {
-        CD3DX12_PIPELINE_STATE_STREAM_ROOT_SIGNATURE pRootSignature;
-        CD3DX12_PIPELINE_STATE_STREAM_CS CS;
-    } pipelineStateStream;
+	// Create the PSO for GenerateMips shader.
+	struct PipelineStateStream
+	{
+		CD3DX12_PIPELINE_STATE_STREAM_ROOT_SIGNATURE pRootSignature;
+		CD3DX12_PIPELINE_STATE_STREAM_CS CS;
+	} pipelineStateStream;
 
-    pipelineStateStream.pRootSignature = m_rootSignature.GetRootSignature().Get();
-    pipelineStateStream.CS = { g_GenerateMips_CS, sizeof( g_GenerateMips_CS ) };
+	pipelineStateStream.pRootSignature = m_rootSignature.GetRootSignature().Get();
+	pipelineStateStream.CS = { g_GenerateMips_CS, sizeof( g_GenerateMips_CS ) };
 
-    D3D12_PIPELINE_STATE_STREAM_DESC pipelineStateStreamDesc = {
-        sizeof( PipelineStateStream ), &pipelineStateStream
-    };
+	D3D12_PIPELINE_STATE_STREAM_DESC pipelineStateStreamDesc = {
+		sizeof( PipelineStateStream ), &pipelineStateStream
+	};
 
-    dxutils::ThrowIfFailed( device->CreatePipelineState( &pipelineStateStreamDesc, IID_PPV_ARGS( &m_pipelineState ) ) );
+	dxutils::ThrowIfFailed( device->CreatePipelineState( &pipelineStateStreamDesc, IID_PPV_ARGS( &m_pipelineState ) ) );
 
-    // Create some default texture UAV's to pad any unused UAV's during mip map generation.
-    m_defaultUAV = dx12Driver->AllocateDescriptors( D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, mipsNum);
-    UINT descriptorHandleIncrementSize = device->GetDescriptorHandleIncrementSize( D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV );
+	// Create some default texture UAV's to pad any unused UAV's during mip map generation.
+	m_defaultUAV = dx12Driver->AllocateDescriptors( D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, mipsNum);
+	UINT descriptorHandleIncrementSize = device->GetDescriptorHandleIncrementSize( D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV );
 
-    for ( UINT i = 0; i < mipsNum; ++i )
-    {
-        D3D12_UNORDERED_ACCESS_VIEW_DESC uavDesc = {};
-        uavDesc.ViewDimension = D3D12_UAV_DIMENSION_TEXTURE2D;
-        uavDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-        uavDesc.Texture2D.MipSlice = i;
-        uavDesc.Texture2D.PlaneSlice = 0;
+	for ( UINT i = 0; i < mipsNum; ++i )
+	{
+		D3D12_UNORDERED_ACCESS_VIEW_DESC uavDesc = {};
+		uavDesc.ViewDimension = D3D12_UAV_DIMENSION_TEXTURE2D;
+		uavDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		uavDesc.Texture2D.MipSlice = i;
+		uavDesc.Texture2D.PlaneSlice = 0;
 
-        device->CreateUnorderedAccessView( nullptr, nullptr, &uavDesc, m_defaultUAV.GetDescriptorHandle(i) );
-    }
+		device->CreateUnorderedAccessView( nullptr, nullptr, &uavDesc, m_defaultUAV.GetDescriptorHandle(i) );
+	}
 }
 
 }
--- snake_ml/system/drivers/win/dx/DX12GenerateMipsPSO.h
+++ snake_ml/system/drivers/win/dx/DX12GenerateMipsPSO.h
@@ -43,43 +43,43 @@ namespace win
 
 struct alignas( 16 ) DX12GenerateMipsCB
 {
-    uint32_t SrcMipLevel;	// Texture level of source mip
-    uint32_t NumMipLevels;	// Number of OutMips to write: [1-4]
-    uint32_t SrcDimension;  // Width and height of the source texture are even or odd.
-    uint32_t Padding;       // Pad to 16 byte alignment.
-    DirectX::XMFLOAT2 TexelSize;	// 1.0 / OutMip1.Dimensions
+	uint32_t SrcMipLevel;	// Texture level of source mip
+	uint32_t NumMipLevels;	// Number of OutMips to write: [1-4]
+	uint32_t SrcDimension;  // Width and height of the source texture are even or odd.
+	uint32_t Padding;	   // Pad to 16 byte alignment.
+	DirectX::XMFLOAT2 TexelSize;	// 1.0 / OutMip1.Dimensions
 };
 
 // I don't use scoped enums to avoid the explicit cast that is required to 
 // treat these as root indices.
 namespace GenerateMips
 {
-    enum
-    {
-        GenerateMipsCB,
-        SrcMip,
-        OutMip,
-        NumRootParameters
-    };
+	enum
+	{
+		GenerateMipsCB,
+		SrcMip,
+		OutMip,
+		NumRootParameters
+	};
 }
 
 class DX12GenerateMipsPSO
 {
 public:
-    DX12GenerateMipsPSO();
-    ~DX12GenerateMipsPSO() = default;
+	DX12GenerateMipsPSO();
+	~DX12GenerateMipsPSO() = default;
 
-    const DX12RootSignature& GetRootSignature() const { return m_rootSignature; }
-    Microsoft::WRL::ComPtr<ID3D12PipelineState> GetPipelineState() const { return m_pipelineState; }
-    D3D12_CPU_DESCRIPTOR_HANDLE GetDefaultUAV() const { return m_defaultUAV.GetDescriptorHandle(); }
+	const DX12RootSignature& GetRootSignature() const { return m_rootSignature; }
+	Microsoft::WRL::ComPtr<ID3D12PipelineState> GetPipelineState() const { return m_pipelineState; }
+	D3D12_CPU_DESCRIPTOR_HANDLE GetDefaultUAV() const { return m_defaultUAV.GetDescriptorHandle(); }
 
 private:
-    DX12RootSignature m_rootSignature;
-    Microsoft::WRL::ComPtr<ID3D12PipelineState> m_pipelineState;
-    // Default (no resource) UAV's to pad the unused UAV descriptors.
-    // If generating less than 4 mip map levels, the unused mip maps
-    // need to be padded with default UAVs (to keep the DX12 runtime happy).
-    DX12DescriptorAllocation m_defaultUAV;
+	DX12RootSignature m_rootSignature;
+	Microsoft::WRL::ComPtr<ID3D12PipelineState> m_pipelineState;
+	// Default (no resource) UAV's to pad the unused UAV descriptors.
+	// If generating less than 4 mip map levels, the unused mip maps
+	// need to be padded with default UAVs (to keep the DX12 runtime happy).
+	DX12DescriptorAllocation m_defaultUAV;
 };
 
 }
