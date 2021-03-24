08afbd4e3fc3bd22de9efb293a6c4ba828cdcce7
alex.mrev1l1.kurinnoi <oleksandr.kurinnoi@gmail.com>
2021-03-24 20:11:48 +0000

    [Refactoring] : Simplified vector definition.

--- snake_ml/utils/math/vector.h
+++ snake_ml/utils/math/vector.h
@@ -5,49 +5,46 @@ namespace snakeml
 namespace math
 {
 
-template<typename T>
 class vector
 {
 public:
 	vector() = default;
-	vector(T _x, T _y, T _z) : x(_x), y(_y), z(_z) {}
+	vector(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
 	~vector() = default;
 
 	float length() const;
-	vector<T> getNormalized() const;
+	vector getNormalized() const;
 	void normalize();
-	vector<T> cross(const vector<T>& a) const;
-	float dot(const vector<T>& a) const;
+	vector cross(const vector& a) const;
+	float dot(const vector& a) const;
 
-	vector<T> operator+(const vector<T>& a) const;
-	void operator+=(const vector<T>& a);
+	vector operator+(const vector& a) const;
+	void operator+=(const vector& a);
 	
-	vector<T> operator-(const vector<T>& a) const;
-	void operator-=(const vector<T>& a);
-	vector<T> operator-();
+	vector operator-(const vector& a) const;
+	void operator-=(const vector& a);
+	vector operator-();
 
-	vector<T> operator*(T scalar) const;
-	void operator*=(T scalar);
+	vector operator*(float scalar) const;
+	void operator*=(float scalar);
 
-	vector<T> operator/(T scalar) const;
-	void operator/(T scalar);
+	vector operator/(float scalar) const;
+	void operator/(float scalar);
 
-	T x = static_cast<T>(0);
-	T y = static_cast<T>(0);
-	T z = static_cast<T>(0);
+	float x = 0.f;
+	float y = 0.f;
+	float z = 0.f;
 
-	static const vector<T> forward	= vector<T> { static_cast<T>(+0), static_cast<T>(+0), static_cast<T>(+1) };
-	static const vector<T> backward	= vector<T> { static_cast<T>(+0), static_cast<T>(+0), static_cast<T>(-1) };
-	static const vector<T> up		= vector<T> { static_cast<T>(+0), static_cast<T>(+1), static_cast<T>(+0) };
-	static const vector<T> down		= vector<T> { static_cast<T>(+0), static_cast<T>(-1), static_cast<T>(+0) };
-	static const vector<T> right	= vector<T> { static_cast<T>(+1), static_cast<T>(+0), static_cast<T>(+0) };
-	static const vector<T> left		= vector<T> { static_cast<T>(-1), static_cast<T>(+0), static_cast<T>(+0) };
-	static const vector<T> zero		= vector<T> { static_cast<T>(+0), static_cast<T>(+0), static_cast<T>(+0) };
+	static const vector forward;
+	static const vector backward;
+	static const vector up;
+	static const vector down;
+	static const vector right;
+	static const vector left;
+	static const vector zero;
 };
 
 #include "vector.inl"
-
-using vectorf = vector<float>;
 
 }
 }
